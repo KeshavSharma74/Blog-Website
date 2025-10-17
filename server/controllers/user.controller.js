4bb9c2c08ae38dc9817809b962c1d1cd9a7053fc
keshavsharma74 <keshav165p@gmail.com>
2025-10-17 14:13:18 +0530

    fixed cookies

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -15,7 +15,7 @@ const generateToken = (userId, res) => {
     res.cookie("jwt", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV !== "development",
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 };
